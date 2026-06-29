a26fa08984401435fb854ff361d4225c541bd9e4
Anthony Paes <antgiraffe@hotmail.com>
2026-06-29 21:15:41 +1000

    add fixture for testing instructions

---
+++ test/virtual-machine/instructions/instructions-set/_fixture.ts
@@ -0,0 +1,39 @@
+import { IInstruction } from "../../../../src/virtual-machine/instructions";
+import { IProcessDefinition, RandomAccessMemory } from "../../../../src/virtual-machine/memory";
+import { CentralProcessingUnit, Process, RegisterType } from "../../../../src/virtual-machine/processor";
+
+export class VirtualMachineFixture {
+
+    public memory: RandomAccessMemory;
+    public cpu: CentralProcessingUnit;
+
+    private process: IProcessDefinition;
+
+    constructor() {
+        this.memory = new RandomAccessMemory(2, 8);
+        this.cpu = new CentralProcessingUnit(this.memory);
+        this.process = this.cpu.createProcess(1)!;
+    }
+
+    public setInstruction(instruction: IInstruction) {
+        const address = (this.process.address >>> 0) + Process.INSTRUCTIONS_OFFSET;
+        const value = instruction.encode();
+        this.memory.writeNumber(address, 2, value);
+    }
+
+    public setRegister(type: RegisterType, number: number, value: number) {
+        const baseAddress = this.process.address >>> 0;
+        const registersOffset = Process.REGISTERS_OFFSETS.get(type)!;
+        const memoryOffset =
+            (type === RegisterType.InstructionPointer || type === RegisterType.StackPointer ? 0 : ((number - (type === RegisterType.Data ? 1 : 0)) * 4));
+        const address = baseAddress + registersOffset + memoryOffset;
+        this.memory.writeNumber(address, 4, value >>> 0);
+    }
+
+    public run() {
+        this.cpu.startProcess(1, 2);
+        this.cpu.performTick();
+    }
+}
+
+
--- test/virtual-machine/instructions/instructions-set/add.spec.ts
+++ test/virtual-machine/instructions/instructions-set/add.spec.ts
@@ -1,5 +1,7 @@
 import { expect } from "chai";
+import { VirtualMachineFixture } from "./_fixture";
 import { InstructionDecoder, InstructionEncoder, AdditionInstruction } from "../../../../src/virtual-machine/instructions";
+import { RegisterType } from "../../../../src/virtual-machine/processor";
 
 describe("add instruction", () => {
     it("can be decoded", () => {
@@ -18,6 +20,34 @@ describe("add instruction", () => {
 
         expect(actual).instanceOf(AdditionInstruction);
         expect(encoded).is.equals(0x444B);
+    });
+
+    it("can add two numbers", () => {
+        const fixture = new VirtualMachineFixture();
+        const instruction = new AdditionInstruction(1, 2, 3);
+
+        fixture.setInstruction(instruction);
+        fixture.setRegister(RegisterType.Data, 1, 2);
+        fixture.setRegister(RegisterType.Data, 2, 3);
+        fixture.run();
+
+        const dump = fixture.cpu.dump();
+
+        expect(dump.registers[0].find(d => d[0] == 'R')![1][2]).to.equal(5);
+    });
+
+    it("can add positive and negative numbers", () => {
+        const fixture = new VirtualMachineFixture();
+        const instruction = new AdditionInstruction(1, 2, 3);
+
+        fixture.setInstruction(instruction);
+        fixture.setRegister(RegisterType.Data, 1, -3);
+        fixture.setRegister(RegisterType.Data, 2, 2);
+        fixture.run();
+
+        const dump = fixture.cpu.dump();
+
+        expect(dump.registers[0].find(d => d[0] == 'R')![1][2]).to.equal(0xFFFFFFFF); // (-1)
     });
 });
 
